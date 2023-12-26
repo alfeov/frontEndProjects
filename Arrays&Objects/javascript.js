4246ec726f80a86115f7bf48cca9554abe2210e2
Diablo_xDSD <alfeove@gmail.com>
2023-12-26 21:43:13 +0300

    Complete chapter 3"Arrays&Objects"

---
+++ Arrays&Objects/javascript.js
@@ -0,0 +1,95 @@
+// 'use strict';
+// let nwarr = ['Milk', 'Bread', 'Apples'];
+
+// nwarr.push('Bananas');
+// nwarr.splice(3, 0, 'Dragon aggs');
+// // nwarr.shift();
+// // delete nwarr[0];
+// console.table(nwarr);
+// // console.log(nwarr.length);
+
+// // let findElement = nwarr.find();
+// // console.log(findElement);
+// // if (findElement !== undefined) {
+// 	// 	console.log(true);
+// 	// }
+	
+// nwarr.push('Milk');
+// // let findElement0 = nwarr.indexOf('Milk', 0);
+// // let findElement1 = nwarr.lastIndexOf('Milk');
+// // console.log(findElement0, findElement1);
+
+
+// // let sort = 
+// nwarr.sort().reverse();
+// console.table(nwarr);
+
+// let test = nwarr.pop();
+// console.log(test);
+
+// 3.4
+// let myCar = {
+// 	make: 'Koenigsegg',
+// 	model: 'Trevita',
+// 	topSpeed_KmH: 400,
+// 	zeroTo100_s: 3.1,
+// 	fullSize: true,
+// 	color: 'gray',
+// 	forSale: true
+// };
+
+// let color = 'forSale';
+
+// myCar[color] = true;
+
+// console.table(myCar);
+// console.log(myCar['make'], myCar['model'], myCar['forSale']);
+
+//! 3.5
+
+// let people = {
+// 	friends: [],
+// };
+
+// let friend1 = {
+// 	ID: 1,
+// 	firstName: 'Anton',
+// 	secondName: 'Ilchevsky',
+// };
+// let friend2 = {
+// 	ID: 2,
+// 	firstName: 'Eugene',
+// 	secondName: 'Dulyk',
+// };
+// let friend3 = {
+// 	ID: 3,
+// 	firstName: 'Ilya',
+// 	secondName: 'Michnuk',
+// };
+
+// people.friends.push(friend1,friend2,friend3);
+
+// console.log(people);
+
+//! 
+
+// const theList = ['Laurence', 'Svekis', true, 35, null, undefined, {test: 'one', score: 55}, ['one', 'two']];
+
+// console.log(theList[6].score);
+// console.log(theList[7][1]);
+
+// theList.shift();
+// theList.pop();
+// theList.pop();
+// theList.pop();
+// theList.pop();
+// theList.pop();
+// theList.pop();
+// theList.unshift('FIRST');
+// theList.push('MIDDLE', 'hello World', 'LAST');
+
+// console.log(theList);
+
+// theList.length = 0;
+// console.log(theList);
+// console.log(theList[21]);
---
+++ Arrays&Objects/javascript3.3.js
@@ -0,0 +1,4 @@
+// let arr123 = [1, 2, 3];
+// let arr = [arr123, arr123, arr123];
+
+// console.log(arr[2][1]);
