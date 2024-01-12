77ed1f095c3fc0c9dc92451ce87d9361593e96e1
Diablo_xDSD <alfeove@gmail.com>
2024-01-13 00:55:57 +0300

    Function(Complete)

--- lessonsJS/from0toProfi/Functions/script.js
+++ lessonsJS/from0toProfi/Functions/script.js
@@ -111,3 +111,117 @@
 // wow();
 // console.log(x);
 //! it works without strict!!!
+
+//? IIFE
+
+// (function IIFE() {console.log('IIFE!')})();
+
+// (() => {console.log('IIFE')})();
+
+// let variable = 1000;
+// let result = (function IIFE() {
+// 	variable = 2000;
+// 	return variable;
+// })();
+// console.log(result);
+// console.log(variable);
+
+// (function (variable) {
+// 	// variable = param;
+// 	console.log('variable is: ' + variable);
+// })(100);
+
+//? recursion
+
+// function recursion (param) {
+// 	if (param === 0) return 1;
+// 	return param * recursion(param - 1);
+// }
+
+// let input = 3;
+
+// console.log(recursion(input));
+
+//? nested functions
+
+// let start = 10;
+// function nested(start) {
+// 	console.log(start);
+// 	if (start < 1) return;
+// 	nested(--start);
+// }
+// nested(start);
+
+
+// function nested_2 (start) {
+// 	console.log(start);
+// 	if (start > 0)
+// 		nested_2(--start);
+// }
+// nested_2(start);
+
+//? anonimus functions
+
+// let funname = function () {
+// 	console.log('x');
+// }
+
+// function callback (fun) {
+// 	fun();
+// 	console.log('Ligging');
+// }
+// callback(funname);
+
+//TODO PROJECTS
+
+//? recursion until 10
+
+// function recursion (start) {
+// 	console.log(start);
+// 	if (start < 10) recursion(++start);
+// 	return;
+// }
+
+// recursion(4);
+
+//? setTimeout()
+
+// let one = () => console.log('one');
+// let two = () => console.log('two');
+
+// let three = () => {
+// 	console.log('three');
+// 	one();
+// 	two();
+// }
+
+// (() => {
+// 	console.log('four');
+// 	setTimeout(three, 0);
+// 	setTimeout(one, 0);
+// })();
+
+
+
+
+
+
+
+// function testf () {
+// 	console.log(test1);
+// 	var test1 = 'test1';
+// }
+// testf();
+// console.log(test1);
+
+// let testFunction = (function() {
+// 	let test = 'test';
+// 	return 'a';
+// })();
+// console.log(testFunction);
+// console.log(test);
+
+// let test2 = (num) => num + 5;
+// console.log(test2(5));
+
+// var test = function test () {}
