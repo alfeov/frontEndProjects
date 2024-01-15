980698d0551fea009048a8d95566c5189df97242
Diablo_xDSD <alfeove@gmail.com>
2024-01-16 00:31:12 +0300

    Build-in Methods complete(Numeric, Date, Consolidation, Projects)

--- lessonsJS/from0toProfi/Build-in Methods/script.js
+++ lessonsJS/from0toProfi/Build-in Methods/script.js
@@ -140,65 +140,175 @@
 //TODO
 
 // let str = 'this word. is amazing? amazing... yes!!! yes...';
-// //! . in the start of converse bug
-// console.log(str);
-
-// str = str.split('.');
-
-// console.table(str);
-
-// function addDots (str) {
-// 	for (let i = 0; i < str.length; i++) {
-// 		if (str[i] !== '') {
-// 			str[i] = str[i].split('');
-// 			str[i].push('.');
-// 			str[i] = str[i].join('');
-	
-// 			console.log(str[i]);
-// 		}
-// 	}
-// 	for (let i = 0; i < str.length - 1; i++) {
-// 		let temp = str.indexOf('');
-
-// 		str[temp-1] = str[temp-1].split('');
-
-// 		let tempEnd = temp + 1;
-// 		while (str[tempEnd] === '') {
-// 			tempEnd++;
-// 		}
-
-// 		if (str[tempEnd + 1] === undefined) {
-// 			for (let j = 0; j < tempEnd - temp - 1; j++) {
-// 				str[temp-1].push('.');
-// 			}
-// 		} else {
-// 			for (let j = 0; j < tempEnd - temp; j++) {
-// 				str[temp-1].push('.');
-// 			}
-// 		}
-
-// 		str[temp-1] = str[temp-1].join('');
-
-// 		str.splice(temp, tempEnd - temp);
-// 	}
-// 	for (let i = 1; i < str.length; i++) {
-// 		str[i] = str[i].split('');
-// 		str[i].shift();
-// 		str[i] = str[i].join('');
-// 	}
-// }
-// addDots(str);
-
-// for (let i = 0; i < str.length; i++) {
-// 	str[i] = str[i].split('?');
-// 	// str[i] = str[i].join('');
-// }
-
 // //!add . ? ! after all-1
 // //!map???
 
 // console.table(str);
-// str = str.join('');
 // console.log(str);
 
 //? numeric methods
+
+// let strange = 'Infinity'; //strinsg is not a number => is not Finite
+// //Infinity, NaN, Undefined = false
+// console.log(isFinite(strange));
+
+// let x = 3, str = 'str';
+// console.log(Number.isInteger(undefined));
+
+// console.log((1/5).toFixed(20));
+// x = (x/7).toFixed(20);
+// console.log(x); //typeof = string but ,xxx n(x) is longer then default valus
+
+// console.log(Number(23000).toPrecision(2));
+
+// console.log(Math.sqrt(Math.min(Math.pow(1, 2), 2, 3)));
+
+// let a = 3.2, b = 5.7;
+// console.log(Math.round(a), Math.ceil(a), Math.floor(a));
+// console.log(Math.round(b), Math.ceil(b), Math.floor(b));
+// let c = -3.2, d = -5.7;
+// console.log(Math.round(c), Math.ceil(c), Math.floor(c));
+// console.log(Math.round(d), Math.ceil(d), Math.floor(d));
+// console.log(Math.trunc(a), Math.trunc(b), 
+// 	Math.trunc(c), Math.trunc(d)
+// );
+
+// console.log(Math.floor(a), Math.floor(b), 
+// 	Math.floor(c), Math.floor(d)
+// );
+
+// console.log((Math.exp(Math.log(2) * 3)).toPrecision(8));
+
+//TODO
+
+// console.log(Math.PI);
+// let x = 5.7;
+// console.log(
+// 	Math.ceil(x),
+// 	Math.floor(x),
+// 	Math.round(x),
+// );
+// function random (repeat) {
+// 	let temp = [];
+// 	for (let i = 0; i < repeat; i++) {
+// 		temp.push(Math.floor(Math.random() * 100) + 1);
+// 	}
+// 	// console.log('Max from iteration: ', Math.max(...temp));
+// 	// console.log('Min from iteration: ', Math.min(...temp));
+// 	console.log('Spread is: ', Math.max(...temp) - Math.min(...temp));
+// }
+
+// let arr = [0, 1, 2];
+// console.log(Math.max(...arr));
+
+// setInterval(random, 1000, 5);
+
+//? date methods
+
+//TODO
+// let date = new Date();
+
+// let yourDateOfBirthdayMONTH = 5;
+// date.setDate(21);
+
+// date.setMonth(yourDateOfBirthdayMONTH - 1);
+// let test = date.getDay();
+// console.log(date, (test + 6) % 7 + 1);
+// let nowYear = date.getFullYear();
+// let arrDaysOfWeek = [0, 1, 2, 3, 4, 5, 6];
+// let arrOfDaysForCheck = [];
+// let currentDayOfWeek;
+// currentDayOfWeek = date.getDay();
+// let previousDayOfWeek = -1;
+
+// for (let i = 0; i < 50; i++) {
+// 	date.setFullYear(nowYear + i);
+// 	currentDayOfWeek = date.getDay();
+// 	if (previousDayOfWeek > currentDayOfWeek) {
+// 		console.log(arrOfDaysForCheck);
+// 		arrDaysOfWeek.forEach((e) => {
+// 			if (arrOfDaysForCheck.indexOf(e) === -1) 
+// 				console.log('Iteration without day: ', (e + 6) % 7 + 1);
+// 		});
+// 		arrOfDaysForCheck = [];
+// 	}
+// 	arrOfDaysForCheck.push(currentDayOfWeek);
+// 	previousDayOfWeek = currentDayOfWeek;
+// 	console.log((currentDayOfWeek + 6) % 7 + 1);
+// }
+//TODO
+
+
+// let date = new Date();
+// date.setMonth(4);
+// console.log(date);
+// let nowMonth = date.getMonth();
+// let monthArr = ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'D'];
+// console.log(monthArr[nowMonth]);
+
+//TODO
+
+// let str = 'string';
+// let newArr = str.split('');
+// let tempRandom;
+// let arrOfNewIndex = [];
+
+// function randomize(max) {
+// 	tempRandom = Math.floor(Math.random() * max);
+// 	if (arrOfNewIndex.indexOf(tempRandom) !== -1) {
+// 		randomize(max);
+// 	}
+// }
+
+// function newIndexArrCreature() {
+// 	for (let i = 0; i < newArr.length; i++) {
+// 		randomize(newArr.length);
+// 		arrOfNewIndex.push(tempRandom);
+// 	}
+// }
+// newIndexArrCreature();
+
+
+// let tempArr = [];
+// for (let i = 0; i < newArr.length; i++) {
+// 	tempArr.push(newArr[arrOfNewIndex[i]]);
+// }
+// newArr = tempArr;
+// newArr = newArr.join('');
+
+// console.log(newArr);
+
+//TODO
+let str = 'string';
+console.log(str = str.substring(0,3));
+// let str = '01.01.2025'
+// let date = Date.parse(str);
+
+// let restTime;
+// let objWithRestTime = {};
+// function rest() {
+// 	restTime = date - new Date();
+// 	let Seconds = Math.floor(restTime/1000);
+// 	let Minutes = Math.floor(Math.floor(restTime/1000)/60);
+// 	let Hours = Math.floor(Math.floor(restTime/1000)/3600);
+// 	let Days = Math.floor(Math.floor(restTime/1000)/86400);
+// 	objWithRestTime = {
+// 		Seconds,
+// 		Minutes,
+// 		Hours,
+// 		Days
+// 	};
+// }
+
+// function print() {
+// 	setTimeout(() => {
+// 		rest();
+// 		for (let key in objWithRestTime) {
+// 			console.log(`${key}: ${objWithRestTime[key]}`);
+// 		}
+// 		print();
+// 	}, 1000)
+// }
+
+// setTimeout(print, 1000);
+
