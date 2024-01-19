7cdfee78c1246ac18eed2bb0a5bb3d03164b4d76
Diablo_xDSD <alfeove@gmail.com>
2024-01-20 01:51:17 +0300

    Interactive&EventListeners not complete (Theory + Project 1), Hangman complete main functionality

--- lessonsJS/from0toProfi/Interactive&EventListeners/script.js
+++ lessonsJS/from0toProfi/Interactive&EventListeners/script.js
@@ -111,26 +111,160 @@ let input1 = document.querySelector('input[name="first"]');
 
 //TODO
 
-const dragme = document.querySelector('#dragme');
+// const dragme = document.querySelector('#dragme');
 
-dragme.addEventListener('dragstart', (e) => {
-	e.target.style.opacity = 0.5;
-})
-dragme.addEventListener('dragend', (e) => {
-	e.target.style.opacity = 1;
-})
-document.querySelectorAll('.box').forEach((each) => {
-	each.addEventListener('dragenter', () => {
-		event.target.classList.add('red');
-	});	
-	each.addEventListener('dragleave', () => {
-		event.target.classList.remove('red');
-	});	
-	each.addEventListener('dragover', () => {
-		event.preventDefault();
-	});
-	each.addEventListener('drop', () => {
-		event.preventDefault();
-		event.target.appendChild(dragme);
-	});
-});
+// dragme.addEventListener('dragstart', (e) => {
+// 	e.target.style.opacity = 0.5;
+// })
+// dragme.addEventListener('dragend', (e) => {
+// 	e.target.style.opacity = 1;
+// })
+
+// document.querySelectorAll('.box').forEach((each) => {
+// 	each.addEventListener('dragenter', () => {
+// 		event.target.classList.add('red');
+// 	});	
+// 	each.addEventListener('dragleave', () => {
+// 		event.target.classList.remove('red');
+// 	});	
+// 	each.addEventListener('dragover', () => {
+// 		event.preventDefault();
+// 	});
+// 	each.addEventListener('drop', () => {
+// 		event.preventDefault();
+// 		event.target.appendChild(dragme);
+// 	});
+// });
+// let output;
+// function valForm() {
+// 	let inputs = event.target.children;
+// 	let error = false;
+// 	output = document.getElementById('output');
+// 	output.innerHTML = '';
+
+// 	if (checker(inputs.firstName)) {
+// 		error = true;
+// 		message('Enter first name!');
+// 	}		
+// 	if (checker(inputs.lastName)) {
+// 		error = true;
+// 		message('Enter last name!');
+// 	}	
+// 	if (checker(inputs.age)) {
+// 		error = true;
+// 		message('Enter age!');
+// 	}		
+// 	if (!checker(inputs.age) && inputs.age.value < 19) {
+// 		error = true;
+// 		message('Age isn\'t correct!');
+// 	}	
+
+// 	if (error) {
+// 		event.preventDefault();
+// 	}
+// 	// // var p = event.target.childNodes;
+// 	// // console.log(p);
+
+// 	// // p.forEach(e => console.log(e));
+// }
+// const checker = function(input) {
+// 	if (input.value.length !== 0) {
+// 		return false;
+// 	}
+// 	return true;
+// }
+// const message = function(msg) {
+// 	console.log(msg);
+// 	output.innerHTML += msg;
+// }
+
+//TODO
+
+// const main = document.querySelector("#block");
+// let mover = { speed: 10, dir: 1, posX: 0 , posY: 0};
+// main.addEventListener("click", moveBlock);
+// main.removeEventListener("click", moveBlock);
+
+// main.addEventListener("click", () => {
+// 	let y = 30;
+// 	setInterval(() => {
+// 		if (y < 1) {
+// 			clearInterval();
+// 		} else {
+// 			moveBlock();
+// 			y--;
+// 		}
+// 	}, 62)
+// });
+
+// function moveBlock() {
+// 	let x = 30;
+// 	let rand = Math.floor(Math.random() * 2);
+// 	console.log(rand);
+// 	setInterval(function () {
+// 		if (x < 1) {
+// 			clearInterval();
+// 		} else {
+// 			if (rand === 0) {
+// 				translateX();
+// 			} else if (rand === 1) {
+// 				translateY();
+// 			}
+// 		}
+// 	}, 2);
+
+// 	function translateX() {
+// 		if (mover.posX > window.innerWidth - 100) {
+// 			mover.posX = window.innerWidth - 100;
+// 			mover.dir *= -1;
+// 		} else if (mover.posX < 0) {
+// 			mover.posX = 0;
+// 			mover.dir *= -1;
+// 		}
+// 		x--;
+// 		mover.posX += x * mover.dir;
+// 		main.style.transform = `translateX(${mover.posX}px) translateY(${mover.posY}px)`;
+// 	}
+	
+// 	function translateY() {
+// 		if (mover.posY > window.innerHeight - 100) {
+// 			mover.posY = window.innerHeight - 100;
+// 			mover.dir *= -1;
+// 		} else if (mover.posY < 0) {
+// 			mover.posY = 0;
+// 			mover.dir *= -1;
+// 		}
+// 		x--;
+// 		mover.posY += x * mover.dir;
+// 		main.style.transform = `translateX(${mover.posX}px) translateY(${mover.posY}px)`;
+// 		console.log(mover.posX, mover.posY)
+// 	}
+// }
+
+//TODO
+
+let boxs = document.querySelectorAll('.box');
+let color = 30;
+for (let i = 0; i< boxs.length; i++) {
+	boxs[i].style.backgroundColor = 	`rgb(${color}, ${color}, ${color})`
+	color += 30;
+}
+
+let section = document.querySelector('#section');
+const history = [];
+
+section.addEventListener('click', tracker);
+
+function tracker() {
+	if (event.target.classList.value) {
+		const tempObj = {}; //! Mutation
+		tempObj.textContent = event.target.textContent;
+		tempObj.id = event.target.id;
+		tempObj.tagName = event.target.tagName;
+		tempObj.className = event.target.classList.value;
+		history.push(tempObj);
+		for (let key of history) {
+			console.log(key);
+		}
+	}
+}
--- lessonsJS/from0toProfi/Projects/Hangman Game/script.js
+++ lessonsJS/from0toProfi/Projects/Hangman Game/script.js
@@ -1,1 +1,104 @@
-let 
+let input = document.querySelector('.input');
+let output = document.querySelector('.wrapper_output');
+let inputValue;
+let counter;
+
+let lifes = document.querySelectorAll('.life');
+let hearts;
+
+function lifesStyle() {
+	let color = 30;
+	for (let i = 0; i < lifes.length; i++) {
+		lifes[i].style.backgroundColor = `rgb(${color}, ${color}, ${color})`
+		color += 20;
+	}
+}
+lifesStyle();
+
+input.addEventListener('keypress', () => {
+		if (event.key === 'Enter') 
+			startGame();
+})
+
+const startGame = () => {
+	if (input.value !== '') {
+		counter = 0;
+		hearts = 6;
+		output.nextElementSibling.style.display = 'none';
+		inputValue = input.value.toUpperCase().split('');
+		input.value = '';
+		outputAdd(inputValue.length);
+	}
+} 
+
+const outputAdd = function(length) {
+	let temp = '';
+	for (let i = 0; i < length; i++) {
+		temp += `<input type="text" class="char" maxlength="1" data-index="${i}">`;
+	}
+	output.innerHTML = temp;
+	inputErrorStyleTrigger();
+}
+
+const inputErrorStyleTrigger = function () {
+	let charset = document.querySelectorAll('.char');
+	charset.forEach((char) => {
+		const removeError = function() {
+			this.classList.remove('outline_error');
+		}
+		char.addEventListener('focus', removeError);
+		char.addEventListener('change', check);
+	})
+}
+
+function check() {
+	if (this.value.toUpperCase() === inputValue[this.dataset.index]) {
+		++counter;
+		this.classList.add('outline_correct');
+		this.disabled = true;
+	} else {
+		--hearts;
+		lifes[hearts].style.opacity = 0;
+		if (hearts === 0) {
+			output.nextElementSibling.innerText = 'GameOver!'
+			output.nextElementSibling.style.display = 'block';
+			document.querySelectorAll('.char').forEach((char) => {
+				char.disabled = true;
+				char.style.color = 'black';
+			});
+			console.log('gemeover');
+		}
+		this.classList.add('outline_error');
+	}
+	if (counter === inputValue.length) {
+		output.nextElementSibling.innerText = 'Congratulations BRO!'
+		output.nextElementSibling.style.display = 'block';
+		console.log('win');
+	}
+}
+
+
+// const outer = (even = 3) => {
+// 	if (even) {
+// 		var test = even;
+// 		inner1();
+// 	}
+// };
+// setTimeout(outer, 0);
+// const inner1 = function() {
+// 	inner2();
+// }
+// const inner2 = function() {
+// 	console.log(test);
+// }
+
+//addEventListener () => {this > window} 
+//addEventListener function func() {this > thiselement}
+//addEventListener function () {this > thiselement}
+//addEventListener test = function () {this > thiselement}
+
+// input.addEventListener('focus', function () {
+// 	const inputValue = this.value;
+// 	console.log(inputValue);
+// })
+
