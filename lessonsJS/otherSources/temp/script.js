2286337d8d745885e6a356f23bb9055d22e9c2cd
Diablo_xDSD <alfeove@gmail.com>
2024-02-06 10:26:49 +0300

    unnecesary, but intermediate

--- lessonsJS/otherSources/temp/script.js
+++ lessonsJS/otherSources/temp/script.js
@@ -1,110 +1,71 @@
 'use strict';
 
-// function recurceFlat(depth, curArr) {
-
-// }
-
-
-
-
-
-function myFlat(depth = 1) {
-	if (isNaN(depth)) {
-		return this;
-	}
+// Array.prototype.myFlat = function myFlat(depth = 1) {
+// 	// if () return this;
 	
-	function flatten(currentArr, depth) {
-		let tempArr = [];
-		if (depth === 0) {
-			currentArr.forEach(each => {
-				if (each) tempArr.push(each);
-			})		
-			return tempArr;
-		}
-		if (depth < 1) {	
-			return currentArr;
-		}
-
-		for (let i = 0; i < currentArr.length; i++) {
-			let el = currentArr[i];
-			if (el instanceof Array) {
-				tempArr.push(...flatten(el, --depth));
-			} else if (el) {
-				tempArr.push(el);
-			}
-		}
-		return tempArr;
-	}
-
-	return flatten(this, depth);
-}
-Array.prototype.myFlat = myFlat;
-
-
-
-
-
-
-// Array.prototype.myFlat = function myFlat(depth) {
-// 	if (isNaN(depth)) {
-// 		return this;
+// 	if (isNaN(depth) || Math.floor(depth) <= 0) {
+// 		return this.filter(x => !!x);
 // 	}
 
-// 	function flatten(curArr, depth) {
-// 		// if (depth === 1) {
-// 		// 	return curArr.filter(x => !!x).flat();
-// 		// }
-// 		if (depth < 1) {
-// 			return curArr.filter(x => !!x)
+// 	function flatten(arr, depth) {
+// 		if (depth < 1 && depth !== 0) {
+// 			return arr;
 // 		}
-
 // 		let tempArr = [];
 
-// 		for (let i = 0; i < curArr.length; i++) {
-// 			let el = curArr[i];
+// 		for (let i = 0; i < arr.length; i++) {
+// 			const el = arr[i];
 // 			if (el instanceof Array) {
-// 				// let tempEl = 
-// 				tempArr.push(...flatten(el, --depth));
+// 				tempArr.push(...flatten(el, --depth)); 
 // 			} else if (el) {
 // 				tempArr.push(el);
 // 			}
 // 		}
+// 		// console.log(tempArr);
 // 		return tempArr;
 // 	}
 
 // 	return flatten(this, depth);
 // }
 
+Array.prototype.myFlat = function(){
+	
+
+	let tempArr = [];
+	if (depth < 1) {
+		for (let i = 0; i < currentArr[i].length; i++) {
+			if (currentArr[i]) tempArr.push(currentArer[i]);
+		}
+		return tempArr;
+	}
+
+	let isFlat = true;
+	for (let i = 0; i < currentArr.length; i++) {
+		if (currentArr[i] instanceof Array) {
+			isFlat = false;
+			for (let j = 0; j < currentArr[i].length; j++) {
+				if (currentArr[i]) tempArr.push(currentArr[i][j]);
+			}
+		} else {
+			if (currentArr[i]) tempArr.push(currentArr[i]);
+		}
+	}
+	if (isFlat) {
+		return tempArr
+	}
 
 
+}
+const inputArray = [0, 1, 2, [3, 4, [5,6]], 7];
+console.log(inputArray.myFlat());
 
 let arr = [1, [2, 3], 1, , 1, [[4]], [5, 6], 7];
+// let arr = [1, ,[2], ,[3]];
+// console.log(arr.myFlat());
+// console.log(arr.flat());
 
-console.log(arr.myFlat(2));
-
-console.log(arr.flat(2))
-console.log(...[4])
+console.log([1, [[2]], , [3]].myFlat());
+console.log([1, [[2]], , [3]].flat());
 
 
-		// let tempArr = [];
-		// if (depth < 1) {
-		// 	for (let i = 0; i < currentArr[i].length; i++) {
-		// 		if (currentArr[i]) tempArr.push(currentArr[i]);
-		// 	}
-		// 	return tempArr;
-		// }
-
-		// let isFlat = true;
-		// for (let i = 0; i < currentArr.length; i++) {
-		// 	if (currentArr[i] instanceof Array) {
-		// 		isFlat = false;
-		// 		for (let j = 0; j < currentArr[i].length; j++) {
-		// 			if (currentArr[i]) tempArr.push(currentArr[i][j]);
-		// 		}
-		// 	} else {
-		// 		if (currentArr[i]) tempArr.push(currentArr[i]);
-		// 	}
-		// }
-		// if (isFlat) {
-		// 	return tempArr
-		// }
+		// d
